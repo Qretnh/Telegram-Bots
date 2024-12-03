2e0aed8e0c627f930207f58e7aa39f40870f59e0
Dubinin Andrey 3382 <144445325+Qretnh@users.noreply.github.com>
2024-12-03 22:05:53 +0300

    Delete keyboards directory

--- keyboards/admin_keyboards.py
+++
@@ -1,7 +0,0 @@
-from aiogram.utils.keyboard import InlineKeyboardBuilder, InlineKeyboardButton, InlineKeyboardMarkup
-from typing import Any
-from settings.prices import set_prices
-
-admin_keyboard = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text="Рассылка", callback_data='sender')]
-])
