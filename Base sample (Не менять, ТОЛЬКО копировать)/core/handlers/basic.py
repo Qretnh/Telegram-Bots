55925902a25f507492cf91a306976bf645f89c38
Dubinin Andrey 3382 <144445325+Qretnh@users.noreply.github.com>
2024-12-15 19:41:09 +0300

    Delete Base sample (Не менять, ТОЛЬКО копировать) directory

--- Base sample (Не менять, ТОЛЬКО копировать)/core/handlers/basic.py
+++
@@ -1,8 +0,0 @@
-from aiogram import Bot
-from aiogram.types import Message
-
-
-async def get_start(message: Message, bot: Bot):
-    await bot.send_message(message.from_user.id, f'<b>привет, {message.from_user.first_name}. Рад тебя видеть!</b>')
-    await message.answer(f'<s>привет, {message.from_user.first_name}. Рад тебя видеть!</s>')
-    await message.reply(f'привет, {message.from_user.first_name}. Рад тебя видеть!')
